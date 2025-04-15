4267bdf4601ce518b54d15d5b74e5c7b41fb29b2
Anas <anas0595571254@gimal>
2025-04-15 18:51:29 +0300

    ja I Make It

---
+++ app/Http/Controllers/SiteOne/SiteOneController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\SiteOne;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SiteOneController extends Controller
+{
+    function home()
+    {
+     return view('SiteOne.home');
+    }
+    function about()
+    {
+        return view('SiteOne.about');
+    }
+    function contact()
+    {
+        return view('SiteOne.contact');
+    }
+    public function postcontact(Request $request)
+    {
+        dd($request);
+    }
+    function msg($id=null)
+    {
+        /*
+        
+        */
+        return view('SiteOne.msg')->with('id',$id);
+        /*
+        Way 2
+        return view('SiteOne.msg',[
+            'id' =>$id
+        ]);
+        */
+
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\SiteOne\SiteOneController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('site1')->name('site1.')->controller(SiteOneController::class)->group(function () {
+    Route::get('/home', 'home')->name('home');
+    Route::get('/about', 'about')->name('about');
+    Route::get('/contact', 'contact')->name('contact');
+    Route::post('/contact', 'postcontact')->name('postcontact');
+   
+});
+
